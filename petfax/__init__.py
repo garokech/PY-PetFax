b38eeee66f30222eea796dbdfc06c5ec798983c5
Garo Kechkarian <garokechkarian@Garos-iMac.local>
2023-01-12 21:30:28 -0800

    New Fact page working

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,12 +1,23 @@
 from flask import Flask 
+from flask_migrate import Migrate
 
 def create_app(): 
     app = Flask(__name__)
-    from . import pet
+    from . import pet, fact, models
+    #config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Aero1998!@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["TEMPLATES_AUTO_RELOAD"] = True    
+
+    #database integration
+    models.db.init_app(app) 
+    migrate = Migrate(app, models.db)
+
+    #blueprint
     app.register_blueprint(pet.bp)
-    from . import fact
     app.register_blueprint(fact.bp)
-    app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
     @app.route('/')
     def hello(): 
         return 'Hello, PetFax!'
--- petfax/fact.py
+++ petfax/fact.py
@@ -1,6 +1,7 @@
-from flask import ( Blueprint, render_template, redirect ) 
+from flask import ( Blueprint, render_template, redirect, request ) 
+from . import models
 
-bp = Blueprint('facet', __name__, url_prefix="/facts")
+bp = Blueprint('facts', __name__, url_prefix="/facts")
 
 @bp.route('/new')
 def new(): 
@@ -9,8 +10,17 @@ def new():
 @bp.route('/', methods=['GET','POST'])
 def index(): 
     if request.method == 'POST':
-       print(request.form)
-       return redirect('/facts')
-    
-    return 'This is the facts index'
+        #red from data filled by user
+        submitter = request.form["submitter"]
+        fact = request.form["fact"]
+        #persist to db
+        new_fact = models.Fact(submitter=submitter,fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
+        return redirect('/facts')
+    #Code for Get Route
+    results = models.Fact.query.all()
+
+    return render_template('facts/index.html', facts=results)
 
