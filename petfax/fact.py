7756a22292c8cdb6f99d8c27605574fbe4f8acb9
Garo Kechkarian <garokechkarian@Garos-iMac.local>
2023-01-12 19:01:26 -0800

    Submit Fact page working.

--- petfax/fact.py
+++ petfax/fact.py
@@ -5,3 +5,8 @@ bp = Blueprint('facet', __name__, url_prefix="/facts")
 @bp.route('/new')
 def new(): 
     return render_template('facts/new.html')
+
+@bp.route('/', methods=['POST'])
+def index():
+    print(request.form)
+    return 'Thanks for submitting that fact!'
